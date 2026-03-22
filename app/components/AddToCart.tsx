74c54ded513f2f078f3889aff8b4d527a5b9d4f5
Grombrindle <everythingiswronj@gmail.com>
2026-03-22 22:07:55 +0300

    DaisyUI

--- app/components/AddToCart.tsx
+++ app/components/AddToCart.tsx
@@ -2,7 +2,11 @@
 import React from "react";
 
 const AddToCart = () => {
-  return <button onClick={() => console.log("s")}>Add to Cart</button>;
+  return (
+    <button className="btn btn-primary" onClick={() => console.log("s")}>
+      Add to Cart
+    </button>
+  );
 };
 
 export default AddToCart;
