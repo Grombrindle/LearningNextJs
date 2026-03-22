f9d3f08a12f42630eb9a775ea6673b08396dc0a2
Grombrindle <everythingiswronj@gmail.com>
2026-03-22 20:29:14 +0300

    tailwind css

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -6,7 +6,8 @@ styles.card;
 
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
+    // <div className={styles.card}>
+    <div className="p-5 my-5 border bg-sky-300  text-white  text-2xl hover:bg-sky-600 rounded">
       <AddToCart />
     </div>
   );
