36d961325977a6d532eccdbc15918386de21a8e5
Grombrindle <everythingiswronj@gmail.com>
2026-03-22 19:04:18 +0300

    Routing and Navigation also learned to use the Link from next/link to make loading things so much faster

---
+++ app/users/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import React from "react";
+
+const UsersPage = () => {
+  return (
+    <>
+      <div>UsersPage</div> <Link href="/users/new">Home</Link>
+    </>
+  );
+};
+
+export default UsersPage;
